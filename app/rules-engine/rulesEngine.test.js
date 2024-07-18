8b486d9463a13ac4b914cef56f0cdb0d6d97ba52
Nick Froud <nfroud@gmail.com>
2024-07-18 15:22:02 +0100

    removed dmn rules engine

--- app/rules-engine/rulesEngine.test.js
+++ app/rules-engine/rulesEngine.test.js
@@ -19,24 +19,4 @@ describe('Rules Engine', function () {
       expect(result).toBe(true)
     })
   })
-
-  // describe('supplementAreaMatchesParent', function () {
-  //   test('DMN - should return true if the area applied for is equal to the parent area', async function () {
-  //     // Arrange
-  //     const application = {
-  //       areaAppliedFor: 100,
-  //       actionCodeAppliedFor: 'GRH7',
-  //       landParcel: {
-  //         existingAgreements: [{ area: 100, code: 'CLIG3' }]
-  //       }
-  //     }
-  //     const rulesEngine = createDmnRulesEngine()
-
-  //     // Act
-  //     const result = await rulesEngine.supplementAreaMatchesParent(application)
-
-  //     // Assert
-  //     expect(result).toBe(true)
-  //   })
-  // })
 })
