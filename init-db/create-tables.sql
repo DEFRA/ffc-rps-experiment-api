f48d89baeda10016249ac4d356f94219d9a8a25f
Bill Sclater <bill.sclater@equalexperts.com>
2024-08-28 22:51:12 +0100

    added actions

--- init-db/create-tables.sql
+++ init-db/create-tables.sql
@@ -2,3 +2,8 @@ CREATE TABLE land_parcels (
     id SERIAL PRIMARY KEY,
     data JSONB NOT NULL
 );
+
+CREATE TABLE actions (
+    id SERIAL PRIMARY KEY,
+    data JSONB NOT NULL
+);
--- init-db/insert-data.sql
+++ init-db/insert-data.sql
@@ -114,3 +114,48 @@ INSERT INTO land_parcels (id, data) VALUES
         "moorlandLineStatus": "below"
     }
 }');
+
+INSERT INTO actions (data) VALUES
+('{
+    "code": "SAM1",
+    "description": "Assess soil, test soil organic matter and produce a soil management plan",
+    "eligibleLandUses": ["Various arable and horticultural land types"],
+    "payment": {
+        "amountPerHectare": 5.80,
+        "additionalPaymentPerAgreement": 95.0
+    }
+}'),
+('{
+    "code": "SAM2",
+    "description": "Multi-species winter cover crop",
+    "eligibleLandUses": ["TG01", "FA01", "TC01"],
+    "payment": {
+        "amountPerHectare": 129.0
+    }
+}'),
+('{
+    "code": "SAM3",
+    "description": "Herbal leys",
+    "eligibleLandUses": ["Arable land", "Temporary grassland", "Arable land lying fallow", "Improved permanent grassland"],
+    "payment": {
+        "amountPerHectare": 382.0
+    }
+}'),
+('{
+    "code": "LIG1",
+    "description": "Manage grassland with very low nutrient inputs (outside SDAs)",
+    "eligibleLandUses": ["Temporary grassland", "Permanent grassland – improved and low input"],
+    "payment": {
+        "amountPerHectare": 151.0
+    }
+}'),
+('{
+    "code": "AB3",
+    "description": "Beetle banks",
+    "eligibleLandUses": ["Temporary grass buffer strip"],
+    "payment": {
+        "amountPerHectare": 573.0
+    },
+    "compatibleActions": ["SAM1"]
+}');
+
