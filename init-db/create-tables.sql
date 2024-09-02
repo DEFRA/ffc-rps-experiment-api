9259f4036e6344352c568982cad8599a16fc12a9
Bill Sclater <bill.sclater@equalexperts.com>
2024-09-02 10:11:41 +0100

    Revert "refactoring"
    This reverts commit cd05fac5a42d54d76ddd408987d797fc6c29a0e4.

--- init-db/create-tables.sql
+++ init-db/create-tables.sql
@@ -1,17 +1,9 @@
--- Drop existing tables if they exist
-DROP TABLE IF EXISTS land_parcels;
-DROP TABLE IF EXISTS actions;
-
--- Create the modified land_parcels table
 CREATE TABLE land_parcels (
-    id SERIAL,
-    sbi VARCHAR(20),
-    parcelId VARCHAR(20),
-    areaHa NUMERIC,
-    sheetId VARCHAR(20),
-    landCovers JSONB,
-    agreements JSONB,
-    landUses JSONB,
-    attributes JSONB,
-    PRIMARY KEY (id, sbi)
+    id SERIAL PRIMARY KEY,
+    data JSONB NOT NULL
+);
+
+CREATE TABLE actions (
+    id SERIAL PRIMARY KEY,
+    data JSONB NOT NULL
 );
