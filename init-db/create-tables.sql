dc9a80ddd3494818e47269dec464fb7b1b008109
Bill Sclater <bill.sclater@equalexperts.com>
2024-09-04 10:14:50 +0100

    enriched data further

--- init-db/create-tables.sql
+++ init-db/create-tables.sql
@@ -1,13 +1,26 @@
-CREATE TABLE land_parcels (
-    id SERIAL PRIMARY KEY,
-    sbi VARCHAR(20),
-    parcelId VARCHAR(20),
-    areaHa NUMERIC,
-    sheetId VARCHAR(20),
-    landCovers JSONB,
-    agreements JSONB,
-    landUses JSONB,
-    attributes JSONB,
-    geometryWkt JSONB,
-    geometry JSONB
+create table land_parcels
+(
+    id               serial
+        primary key,
+    sbi              varchar(20),
+    parcelId         varchar(20),
+    areaHa           numeric,
+    sheetId          varchar(20),
+    landCovers       jsonb,
+    agreements       jsonb,
+    landUses         jsonb,
+    attributes       jsonb,
+    geometryWkt      jsonb,
+    geometry         jsonb,
+    centroidX        numeric,
+    centroidY        numeric,
+    validFrom        timestamp,
+    validTo          timestamp,
+    geomAreaSqm      numeric,
+    verifiedOn       timestamp,
+    verificationtype integer,
+    createdOn        timestamp,
+    createdBy        varchar(255),
+    validated        char,
+    lastRefreshDate  timestamp
 );
