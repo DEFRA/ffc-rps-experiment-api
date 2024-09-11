019a6520e4fc7a9a44b4346c349d19c4e942c101
Bill Sclater <bill.sclater@equalexperts.com>
2024-09-11 16:28:30 +0100

    test refactor

--- test/integration/narrow/routes/action-rule.test.js
+++ test/integration/narrow/routes/action-rule.test.js
@@ -1,64 +1,9 @@
-const { initActionsCache, getAction, getActions, addRule, findRuleIndex, updateRule, deleteRule } = require('../../../../app/land-action')
-
-jest.mock('../../../../app/land-action', () => {
-  const originalModule = jest.requireActual('../../../../app/land-action')
-  return {
-    ...originalModule,
-    initActionsCache: jest.fn(),
-    getAction: jest.fn(),
-    getActions: jest.fn(),
-    addRule: jest.fn(),
-    findRuleIndex: jest.fn(),
-    updateRule: jest.fn(),
-    deleteRule: jest.fn()
-  }
-})
+const { getAction } = require('../../../../app/land-action')
 
 describe('action rule routes', () => {
   const server = require('../../../../app/server')
-  let actions
 
   beforeEach(async () => {
-    actions = require('../../../../app/static-data/actions.json')
-    initActionsCache.mockImplementation(() => {
-      actions = JSON.parse(JSON.stringify(require('../../../../app/static-data/actions.json')))
-    })
-    getAction.mockImplementation((code) => actions.find(action => action.code === code))
-    getActions.mockImplementation(() => actions)
-    addRule.mockImplementation((actionObject, newRule) => {
-      if (!actionObject.eligibilityRules) {
-        actionObject.eligibilityRules = []
-      }
-      if (!actionObject.eligibilityRules.some(rule => rule.id === newRule.id)) {
-        actionObject.eligibilityRules.push(newRule)
-      }
-    })
-    findRuleIndex.mockImplementation((eligibilityRules, ruleId) => eligibilityRules.findIndex(rule => rule.id === ruleId))
-    updateRule.mockImplementation((action, ruleToUpdate) => {
-      const ruleIndex = findRuleIndex(action.eligibilityRules, ruleToUpdate.id)
-      if (ruleIndex === -1) {
-        return false
-      }
-      action.eligibilityRules[ruleIndex] = ruleToUpdate
-      const actionIndex = actions.findIndex(a => a.code === action.code)
-      if (actionIndex !== -1) {
-        actions[actionIndex] = action
-      }
-      return true
-    })
-    deleteRule.mockImplementation((action, id) => {
-      const ruleIndex = findRuleIndex(action.eligibilityRules, id)
-      if (ruleIndex === -1) {
-        return false
-      }
-      action.eligibilityRules.splice(ruleIndex, 1)
-      const actionIndex = actions.findIndex(a => a.code === action.code)
-      if (actionIndex !== -1) {
-        actions[actionIndex] = action
-      }
-      return true
-    })
-    initActionsCache()
     await server.start()
   })
 
