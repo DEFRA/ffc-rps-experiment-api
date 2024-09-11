2c671f7c0e31777cba43d4ef73af33b5ec21aab0
Bill Sclater <bill.sclater@equalexperts.com>
2024-09-11 09:13:25 +0100

    improved test coverage

--- test/unit/land-parcel/index.test.js
+++ test/unit/land-parcel/index.test.js
@@ -1,21 +1,27 @@
-const { getAction, getActions } = require('../../../app/land-action')
+const { getAction, getActions, addRule, findRuleIndex, updateRule, deleteRule } = require('../../../app/land-action')
 
 describe('get land actions', () => {
   test('should return an array of all land actions', () => {
     const actions = getActions()
     expect(actions.length).toEqual(7)
   })
+
   test('should return a land action when a valid action code is provided', () => {
     const action = getAction('SAM1')
     expect(action.code).toEqual('SAM1')
     expect(action.description).toEqual('Assess soil, test soil organic matter and produce a soil management plan')
   })
+
+  test('should return undefined when an invalid action code is provided', () => {
+    const action = getAction('INVALID_CODE')
+    expect(action).toBeUndefined()
+  })
 })
 
 describe('land action rules', () => {
-  let getAction, getActions, addRule, findRuleIndex, updateRule, deleteRule, initActionsCache
-
   beforeEach(() => {
+    jest.resetModules()
+
     jest.mock('../../../app/land-action', () => {
       const originalModule = jest.requireActual('../../../app/land-action')
       return {
@@ -31,25 +37,21 @@ describe('land action rules', () => {
     })
 
     const landAction = require('../../../app/land-action')
-    getAction = landAction.getAction
-    getActions = landAction.getActions
-    addRule = landAction.addRule
-    findRuleIndex = landAction.findRuleIndex
-    updateRule = landAction.updateRule
-    deleteRule = landAction.deleteRule
-    initActionsCache = landAction.initActionsCache
 
+    // Mock data
     const mockActions = [
-      { code: 'SAM1', eligibilityRules: [{ id: 'is-below-moorland-line', config: {} }] }
+      { code: 'SAM1', eligibilityRules: [{ id: 'is-below-moorland-line', config: {} }] },
+      { code: 'SAM2', eligibilityRules: [] } // Reset to ensure it's empty initially
     ]
 
-    initActionsCache.mockImplementation(() => {
+    // Mock implementations
+    landAction.initActionsCache.mockImplementation(() => {
       landAction.actions = mockActions
     })
 
-    getAction.mockImplementation((code) => mockActions.find(action => action.code === code))
-    getActions.mockImplementation(() => mockActions)
-    addRule.mockImplementation((action, newRule) => {
+    landAction.getAction.mockImplementation((code) => mockActions.find(action => action.code === code))
+    landAction.getActions.mockImplementation(() => mockActions)
+    landAction.addRule.mockImplementation((action, newRule) => {
       if (!action.eligibilityRules) {
         action.eligibilityRules = []
       }
@@ -57,15 +59,15 @@ describe('land action rules', () => {
         action.eligibilityRules.push(newRule)
       }
     })
-    findRuleIndex.mockImplementation((eligibilityRules, ruleId) => eligibilityRules.findIndex(rule => rule.id === ruleId))
-    updateRule.mockImplementation((action, ruleIndex, newRule) => {
+    landAction.findRuleIndex.mockImplementation((eligibilityRules, ruleId) => eligibilityRules.findIndex(rule => rule.id === ruleId))
+    landAction.updateRule.mockImplementation((action, ruleIndex, newRule) => {
       action.eligibilityRules[ruleIndex] = newRule
     })
-    deleteRule.mockImplementation((action, ruleIndex) => {
+    landAction.deleteRule.mockImplementation((action, ruleIndex) => {
       action.eligibilityRules.splice(ruleIndex, 1)
     })
 
-    initActionsCache()
+    landAction.initActionsCache() // Reinitialize the cache before each test
   })
 
   test('should add a rule to a land action', () => {
@@ -73,6 +75,16 @@ describe('land action rules', () => {
     const newRule = { id: 'new-rule', config: {} }
     addRule(action, newRule)
     expect(action.eligibilityRules.some(rule => rule.id === 'new-rule')).toBe(true)
+  })
+
+  test('should initialize eligibilityRules if undefined when adding a rule', () => {
+    const action = getAction('SAM2')
+    const newRule = { id: 'another-new-rule', config: {} }
+    action.eligibilityRules = undefined
+    addRule(action, newRule)
+    expect(action.eligibilityRules).toBeDefined()
+    expect(action.eligibilityRules.length).toBe(1)
+    expect(action.eligibilityRules[0].id).toBe('another-new-rule')
   })
 
   test('should not add a duplicate rule to a land action', () => {
@@ -102,6 +114,13 @@ describe('land action rules', () => {
     expect(action.eligibilityRules[ruleIndex].config.updated).toBe(true)
   })
 
+  test('should not update any rule if rule index is invalid', () => {
+    const action = getAction('SAM1')
+    const ruleIndex = -1
+    const updatedRule = { id: 'non-existent-rule', config: { updated: true } }
+    expect(() => updateRule(action, ruleIndex, updatedRule)).not.toThrow()
+  })
+
   test('should delete a rule from a land action', () => {
     const action = getAction('SAM1')
     const ruleIndex = findRuleIndex(action.eligibilityRules, 'is-below-moorland-line')
@@ -113,5 +132,21 @@ describe('land action rules', () => {
     const action = getAction('SAM1')
     const ruleIndex = findRuleIndex(action.eligibilityRules, 'non-existent-rule')
     expect(() => deleteRule(action, ruleIndex)).not.toThrow()
+  })
+
+  test('should update actions array after adding a new rule', () => {
+    const action = getAction('SAM1')
+    const newRule = { id: 'new-action-rule', config: {} }
+    addRule(action, newRule)
+    const updatedAction = getAction('SAM1')
+    expect(updatedAction.eligibilityRules.some(rule => rule.id === 'new-action-rule')).toBe(true)
+  })
+
+  test('should update actions array after deleting a rule', () => {
+    const action = getAction('SAM1')
+    const ruleIndex = findRuleIndex(action.eligibilityRules, 'is-below-moorland-line')
+    deleteRule(action, ruleIndex)
+    const updatedAction = getAction('SAM1')
+    expect(updatedAction.eligibilityRules.some(rule => rule.id === 'is-below-moorland-line')).toBe(false)
   })
 })
