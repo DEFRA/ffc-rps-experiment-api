bbce472b4b47045ec7acbeb997c50d689d4a2c50
Bill Sclater <bill.sclater@equalexperts.com>
2024-09-11 08:42:25 +0100

    test coverage

--- test/unit/land-parcel/index.test.js
+++ test/unit/land-parcel/index.test.js
@@ -1,41 +1,70 @@
-jest.mock('../../../app/land-action', () => {
-  const landAction = jest.requireActual('../../../app/land-action')
-  return {
-    ...landAction,
-    initActionsCache: jest.fn()
-  }
-})
-
-const { getAction, getActions, addRule, findRuleIndex, updateRule, deleteRule, initActionsCache } = require('../../../app/land-action')
+const { getAction, getActions } = require('../../../app/land-action')
 
 describe('get land actions', () => {
   test('should return an array of all land actions', () => {
     const actions = getActions()
     expect(actions.length).toEqual(7)
   })
-
   test('should return a land action when a valid action code is provided', () => {
     const action = getAction('SAM1')
     expect(action.code).toEqual('SAM1')
     expect(action.description).toEqual('Assess soil, test soil organic matter and produce a soil management plan')
   })
-
-  test('should return undefined when an invalid action code is provided', () => {
-    const action = getAction('INVALID_CODE')
-    expect(action).toBeUndefined()
-  })
 })
 
 describe('land action rules', () => {
-  let mockActions
+  let getAction, getActions, addRule, findRuleIndex, updateRule, deleteRule, initActionsCache
 
   beforeEach(() => {
-    mockActions = [
+    jest.mock('../../../app/land-action', () => {
+      const originalModule = jest.requireActual('../../../app/land-action')
+      return {
+        ...originalModule,
+        initActionsCache: jest.fn(),
+        getAction: jest.fn(),
+        getActions: jest.fn(),
+        addRule: jest.fn(),
+        findRuleIndex: jest.fn(),
+        updateRule: jest.fn(),
+        deleteRule: jest.fn()
+      }
+    })
+
+    const landAction = require('../../../app/land-action')
+    getAction = landAction.getAction
+    getActions = landAction.getActions
+    addRule = landAction.addRule
+    findRuleIndex = landAction.findRuleIndex
+    updateRule = landAction.updateRule
+    deleteRule = landAction.deleteRule
+    initActionsCache = landAction.initActionsCache
+
+    const mockActions = [
       { code: 'SAM1', eligibilityRules: [{ id: 'is-below-moorland-line', config: {} }] }
     ]
+
     initActionsCache.mockImplementation(() => {
-      require('../../../app/land-action').actions = mockActions
+      landAction.actions = mockActions
     })
+
+    getAction.mockImplementation((code) => mockActions.find(action => action.code === code))
+    getActions.mockImplementation(() => mockActions)
+    addRule.mockImplementation((action, newRule) => {
+      if (!action.eligibilityRules) {
+        action.eligibilityRules = []
+      }
+      if (!action.eligibilityRules.some(rule => rule.id === newRule.id)) {
+        action.eligibilityRules.push(newRule)
+      }
+    })
+    findRuleIndex.mockImplementation((eligibilityRules, ruleId) => eligibilityRules.findIndex(rule => rule.id === ruleId))
+    updateRule.mockImplementation((action, ruleIndex, newRule) => {
+      action.eligibilityRules[ruleIndex] = newRule
+    })
+    deleteRule.mockImplementation((action, ruleIndex) => {
+      action.eligibilityRules.splice(ruleIndex, 1)
+    })
+
     initActionsCache()
   })
 
@@ -48,9 +77,9 @@ describe('land action rules', () => {
 
   test('should not add a duplicate rule to a land action', () => {
     const action = getAction('SAM1')
-    const newRule = { id: 'is-below-moorland-line', config: {} } // Same ID as existing rule
+    const newRule = { id: 'is-below-moorland-line', config: {} }
     addRule(action, newRule)
-    expect(action.eligibilityRules.length).toBe(1) // Length should remain the same
+    expect(action.eligibilityRules.filter(rule => rule.id === 'is-below-moorland-line').length).toBe(1)
   })
 
   test('should find the index of a rule in eligibilityRules', () => {
@@ -59,10 +88,10 @@ describe('land action rules', () => {
     expect(ruleIndex).toBeGreaterThanOrEqual(0)
   })
 
-  test('should return -1 for a non-existent rule', () => {
+  test('should return -1 when rule is not found in eligibilityRules', () => {
     const action = getAction('SAM1')
     const ruleIndex = findRuleIndex(action.eligibilityRules, 'non-existent-rule')
-    expect(ruleIndex).toEqual(-1)
+    expect(ruleIndex).toBe(-1)
   })
 
   test('should update a rule in a land action', () => {
@@ -73,14 +102,6 @@ describe('land action rules', () => {
     expect(action.eligibilityRules[ruleIndex].config.updated).toBe(true)
   })
 
-  test('should handle updating a rule with an invalid index', () => {
-    const action = getAction('SAM1')
-    const ruleIndex = -1 // Invalid index
-    const updatedRule = { id: 'is-below-moorland-line', config: { updated: true } }
-    updateRule(action, ruleIndex, updatedRule)
-    expect(action.eligibilityRules.some(rule => rule.config.updated)).toBe(false) // No updates should be made
-  })
-
   test('should delete a rule from a land action', () => {
     const action = getAction('SAM1')
     const ruleIndex = findRuleIndex(action.eligibilityRules, 'is-below-moorland-line')
@@ -88,10 +109,9 @@ describe('land action rules', () => {
     expect(action.eligibilityRules.some(rule => rule.id === 'is-below-moorland-line')).toBe(false)
   })
 
-  test('should handle deleting a rule with an invalid index', () => {
+  test('should not throw an error when deleting a non-existent rule', () => {
     const action = getAction('SAM1')
-    const ruleIndex = -1 // Invalid index
-    deleteRule(action, ruleIndex)
-    expect(action.eligibilityRules.length).toBe(1) // No deletion should occur
+    const ruleIndex = findRuleIndex(action.eligibilityRules, 'non-existent-rule')
+    expect(() => deleteRule(action, ruleIndex)).not.toThrow()
   })
 })
